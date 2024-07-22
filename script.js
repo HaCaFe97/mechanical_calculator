bcb6a95ef32f2a1ec686d6053a0b09543d0aa92e
Harry <hacafe97@gmail.com>
2024-07-22 13:18:40 -0500

    Se cambió nombre del archivo de materiales, especificando que se trata de aleaciones de acero. Se modificó la estrucutra del .json

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('materials.json')
+    fetch('carbon_and_alloy_steels.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
